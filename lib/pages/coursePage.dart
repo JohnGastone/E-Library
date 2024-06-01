cbe93cabf3feec18734c5fc0370849ac5834fd62
JohnGastone <johngastone11@gmail.com>
2024-06-01 20:18:20 +0300

    +++

---
+++ lib/pages/coursePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Coursepagess extends StatefulWidget {
+  const Coursepagess({super.key});
+
+  @override
+  State<Coursepagess> createState() => _CoursepagessState();
+}
+
+class _CoursepagessState extends State<Coursepagess> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
