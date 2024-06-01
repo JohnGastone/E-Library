e6ad16f9dc9fd406dcd24747d016351bd777d480
JohnGastone <johngastone11@gmail.com>
2024-06-01 19:51:55 +0300

    +++

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, prefer_const_constructors, use_key_in_widget_constructors, library_private_types_in_public_api
 
 import 'package:elearning/pages/homePage.dart';
 import 'package:elearning/pages/signupPage.dart';
@@ -250,6 +250,9 @@ class _LoginPageState extends State<LoginPage> {
                     ],
                   ),
                 ),
+                SizedBox(
+                  height: 60,
+                )
               ],
             ),
           ),
