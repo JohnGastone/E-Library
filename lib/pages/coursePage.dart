93de50a2cf448352e35c45ccef8d711234a0908d
JohnGastone <johngastone11@gmail.com>
2024-06-02 17:32:49 +0300

    ++++

--- lib/pages/coursePage.dart
+++ lib/pages/coursePage.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:elearning/pages/homePage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Coursepage extends StatefulWidget {
   const Coursepage({super.key});
@@ -13,6 +15,28 @@ class _CoursepagessState extends State<Coursepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white24,
+        title: Row(
+          children: [
+            FloatingActionButton(
+                child: Text(
+                  "Go Back",
+                  style: GoogleFonts.spaceMono(fontSize: 15),
+                ),
+                onPressed: () {
+                  Navigator.pop(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => AvailableCourses(
+                                isDarkTheme: false,
+                                onThemeChanged: (value) {},
+                              )));
+                })
+          ],
+        ),
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [],
