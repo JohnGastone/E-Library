e4cbf3d4b7fecb93fc1b7db1bf4787773696a643
JohnGastone <johngastone11@gmail.com>
2024-06-03 09:00:41 +0300

    ++++

--- lib/models/courseCard_model.dart
+++ lib/models/courseCard_model.dart
@@ -8,7 +8,41 @@ class CoursecardModel {
       this.courseVideos);
 }
 
-class CourseCardList {
+class CourseCardCsList {
+  static List<CoursecardModel> getCourseCards = [
+    CoursecardModel("assets/images/courses/course-1.jpg", "Course 1",
+        "100 Sessions", "100 Videos"),
+    CoursecardModel("assets/images/courses/course-2.jpg", "Course 2",
+        "100 Sessions", "100 Videos"),
+    CoursecardModel("assets/images/courses/course-3.jpg", "Course 3",
+        "100 Sessions", "100 Videos"),
+    CoursecardModel("assets/images/courses/course-4.jpg", "Course 4",
+        "100 Sessions", "100 Videos"),
+    CoursecardModel("assets/images/courses/course-3.jpg", "Course 3",
+        "100 Sessions", "100 Videos"),
+    CoursecardModel("assets/images/courses/course-4.jpg", "Course 4",
+        "100 Sessions", "100 Videos"),
+  ];
+}
+
+class CourseCardBsList {
+  static List<CoursecardModel> getCourseCards = [
+    CoursecardModel("assets/images/courses/course-1.jpg", "Course 1",
+        "100 Sessions", "100 Videos"),
+    CoursecardModel("assets/images/courses/course-2.jpg", "Course 2",
+        "100 Sessions", "100 Videos"),
+    CoursecardModel("assets/images/courses/course-3.jpg", "Course 3",
+        "100 Sessions", "100 Videos"),
+    CoursecardModel("assets/images/courses/course-4.jpg", "Course 4",
+        "100 Sessions", "100 Videos"),
+    CoursecardModel("assets/images/courses/course-3.jpg", "Course 3",
+        "100 Sessions", "100 Videos"),
+    CoursecardModel("assets/images/courses/course-4.jpg", "Course 4",
+        "100 Sessions", "100 Videos"),
+  ];
+}
+
+class CourseCardHsList {
   static List<CoursecardModel> getCourseCards = [
     CoursecardModel("assets/images/courses/course-1.jpg", "Course 1",
         "100 Sessions", "100 Videos"),
