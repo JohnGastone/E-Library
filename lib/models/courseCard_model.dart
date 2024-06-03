799e3e92e53e6e3130aa5770102c3eb73d11fb94
JohnGastone <johngastone11@gmail.com>
2024-06-03 08:56:03 +0300

    ++++

--- lib/models/courseCard_model.dart
+++ lib/models/courseCard_model.dart
@@ -1,1 +1,9 @@
-class CoursecardModel {}
+class CoursecardModel {
+  String? courseImage;
+  String? courseName;
+  String? courseSessions;
+  String? courseVideos;
+
+  CoursecardModel(this.courseImage, this.courseName, this.courseSessions,
+      this.courseVideos);
+}
