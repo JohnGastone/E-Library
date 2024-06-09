6127ac76b8ad6ed13689a82300639a4080bb4624
JohnGastone <johngastone11@gmail.com>
2024-06-09 14:51:18 +0300

    +++

---
+++ lib/pages/departmentsPage.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Departmentspage extends StatefulWidget {
+  const Departmentspage({super.key});
+
+  @override
+  State<Departmentspage> createState() => _DepartmentspageState();
+}
+
+class _DepartmentspageState extends State<Departmentspage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
