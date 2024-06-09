5a7356780db4b80917ac5b79dcb9b10fe1e26fa7
JohnGastone <johngastone11@gmail.com>
2024-06-09 18:10:01 +0300

    +++

--- lib/pages/moduleUploadPage.dart
+++ lib/pages/moduleUploadPage.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../models/courseModules_model.dart';
 
 class Moduleuploadpage extends StatefulWidget {
   const Moduleuploadpage({super.key});
@@ -10,14 +14,71 @@ class Moduleuploadpage extends StatefulWidget {
 }
 
 class _ModuleuploadpageState extends State<Moduleuploadpage> {
+  List<CoursemodulesModel> displayModules =
+      List.from(CourseModulesList.displayModules);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [],
-          ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    "Uploaded Materials",
+                    style: GoogleFonts.spaceMono(
+                        fontSize: 22, fontWeight: FontWeight.w800),
+                  ),
+                  SizedBox(height: 20),
+                  ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: displayModules.length,
+                      itemBuilder: (context, index) => Padding(
+                            padding: const EdgeInsets.only(
+                                left: 10, right: 10, top: 15),
+                            child: Container(
+                              height: 50,
+                              width: 300,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color:
+                                      const Color.fromARGB(255, 110, 140, 163)),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  children: [
+                                    Icon(CupertinoIcons.circle),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(
+                                      displayModules[index].moduleName!,
+                                      style: GoogleFonts.spaceMono(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                    Spacer(),
+                                    Align(
+                                      alignment: Alignment.centerRight,
+                                      child: Icon(Icons.file_download),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )),
+                  SizedBox(
+                    height: 40,
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
